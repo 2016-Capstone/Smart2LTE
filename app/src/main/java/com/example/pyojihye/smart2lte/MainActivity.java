696b09d8d32400094b143f5269130047b406e3d9
JIHYEPYO <pyojihye95@gmail.com>
2017-03-14 21:46:44 +0900

    LTE

--- app/src/main/java/com/example/pyojihye/smart2lte/MainActivity.java
+++ app/src/main/java/com/example/pyojihye/smart2lte/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -25,10 +26,10 @@ import static com.example.pyojihye.smart2lte.Const.PORT;
 
 public class MainActivity extends AppCompatActivity {
 
-    final private int MY_PERMISSIONS_REQUEST_READ_CONTACTS=1;
+    final private int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
 
     private EditText editTextIP;
-    private ToggleButton toggleButtonConnect;
+    private Button buttonConnect;
 
     private Handler handler;
 
@@ -37,41 +38,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editTextIP = (EditText) findViewById(R.id.editTextIP);
-        toggleButtonConnect=(ToggleButton)findViewById(R.id.toggleButtonConnect);
+        buttonConnect = (Button) findViewById(R.id.buttonConnect);
     }
 
-    public void onToggleButtonConnectClicked(View v){
-        if(((ToggleButton)v).isChecked()){ //off->on
-            int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+    public void onButtonConnectClicked(View v) {
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
 
-            if(permissionCheck==-1){
-                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+        if (permissionCheck == -1) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+        }
+
+        if (editTextIP.getText().toString().equals("")) {
+            Toast.makeText(getApplicationContext(), getString(R.string.toast_ip), Toast.LENGTH_LONG).show();
+        } else {
+            handler = new Handler();
+
+            try {
+                setSocket(editTextIP.getText().toString());
+                Intent intentFlight = new Intent(getApplicationContext(), FlightActivity.class);
+                intentFlight.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intentFlight);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
-            if(editTextIP.getText().toString().equals("")){
-                Toast.makeText(getApplicationContext(),getString(R.string.toast_ip), Toast.LENGTH_LONG).show();
-            }else{
-                handler = new Handler();
-
-                try{
-                    setSocket(editTextIP.getText().toString());
-                    Intent intentFlight = new Intent(getApplicationContext(), FlightActivity.class);
-                    intentFlight.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(intentFlight);
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }
-
-
-        }else{ //on->off
 
         }
     }
 
     public void setSocket(String editTextIP) throws IOException {
-        IP = editTextIP.substring(0,editTextIP.lastIndexOf(":"));
-        PORT = Integer.parseInt(editTextIP.substring(editTextIP.lastIndexOf(":")+1,editTextIP.length()));
+        IP = editTextIP.substring(0, editTextIP.lastIndexOf(":"));
+        PORT = Integer.parseInt(editTextIP.substring(editTextIP.lastIndexOf(":") + 1, editTextIP.length()));
     }
 
 
